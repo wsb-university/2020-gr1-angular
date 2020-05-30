00a867166c772c5ee19457cf109bf72eb5400dac
medi71 <wojciech@medynski.io>
2020-05-30 12:02:27 +0200

    Working routing

--- gallery-app/src/app/app.module.ts
+++ gallery-app/src/app/app.module.ts
@@ -2,8 +2,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { ApiHelper } from './api-helper.service';
 import { AppComponent } from './app.component';
+import { CelsiusSuffixPipe } from './celsius-suffix.pipe';
+import { CelsiusPipe } from './celsius.pipe';
 import { ForecastComponent } from './forecast/forecast.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { HeaderComponent } from './header/header.component';
@@ -11,8 +14,21 @@ import { HelloComponent } from './hello/hello.component';
 import { ImageComponent } from './image/image.component';
 import { TestComponent } from './test.component';
 import { WeatherComponent } from './weather/weather.component';
-import { CelsiusPipe } from './celsius.pipe';
-import { CelsiusSuffixPipe } from './celsius-suffix.pipe';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: WeatherComponent,
+    },
+    {
+        path: 'forecast',
+        component: ForecastComponent,
+    },
+    {
+        path: 'gallery',
+        component: GalleryComponent,
+    },
+];
 
 @NgModule({
     declarations: [
@@ -27,7 +43,12 @@ import { CelsiusSuffixPipe } from './celsius-suffix.pipe';
         CelsiusPipe,
         CelsiusSuffixPipe,
     ],
-    imports: [BrowserModule, FormsModule, HttpClientModule],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        RouterModule.forRoot(routes),
+    ],
     providers: [ApiHelper],
     bootstrap: [AppComponent],
 })
